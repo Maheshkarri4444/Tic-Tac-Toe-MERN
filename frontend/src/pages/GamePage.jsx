d7222bf3ee11a90b366b8f7498b84a7a0cb9f7f7
Maheshkarri4444 <maheshkarri2109@gmail.com>
2024-11-18 23:49:39 +0530

    a commit

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Turn from '../components/Turn'
 import GameBox from '../components/GameBox'
 
 const GamePage = () => {
   return (
     <div>
-      <Turn/>
       <GameBox/>
     </div>
   )
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const HomePage = () => {
   return (
     <div className='flex flex-col items-center justify-center w-full h-full gap-6'>
-        <button id="play-again"  className=' cursor-pointer bg-[#FF2E63] text-[1.2rem] px-[25px] py-[10px] rounded-md border-none hover:bg-[#08D9D6] hover:text-black hover:px-[40px]' >Offline Mode</button>
-        <Link to="/online" id="online-mode"  className=' cursor-pointer bg-[#FF2E63] text-[1.2rem] px-[25px] py-[10px] rounded-md border-none hover:bg-[#08D9D6] hover:text-black hover:px-[40px]' >Online Mode</Link>
+        <button id="play-again"  className=' cursor-pointer bg-indigo-600 text-[1.2rem] px-[25px] py-[10px] rounded-md border-none hover:bg-indigo-700  hover:px-[40px]' >Offline Mode</button>
+        <Link to="/online" id="online-mode"  className=' cursor-pointer bg-indigo-600 text-[1.2rem] px-[25px] py-[10px] rounded-md border-none hover:bg-indigo-700  hover:px-[40px]' >Online Mode</Link>
     </div>
   )
 }
